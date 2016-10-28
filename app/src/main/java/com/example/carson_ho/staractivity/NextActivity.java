e748c5a3f6f8863d5c9c997a21fccebc9f37fe5d
Carson.Ho <carson.ho@foxmail.com>
2016-10-28 10:22:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/carson_ho/staractivity/NextActivity.java
@@ -0,0 +1,16 @@
+package com.example.carson_ho.staractivity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Carson_Ho on 16/10/17.
+ */
+public class NextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+    }
+}
